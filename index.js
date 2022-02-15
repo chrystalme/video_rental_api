c8338684d4bccee13630539de53dd3e1f5fa8184
chrystalme <afam_ifeanyi@live.com>
2022-02-15 09:20:12 +0200

    Add authentication, User model, JWT, bcrypt and config

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+const config = require('config');
 const mongoose = require("mongoose");
 const express = require("express");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
 const app = express();
+
+if (!config.get('JwtSecretKey')) {
+  console.error('FATAL ERROR: jwtSecretKey is not defined.');
+  process.exit(1);
+}
+// console.log(config.get("JwtSecretKey"));
 
 main().catch(err => console.log('could not connect to mongoDB', err.message));
 
@@ -19,6 +28,8 @@ app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 const port = process.env.POST || 3000;
 app.listen(port, () => console.log(`listening on port ${port}...`));
--- routes/movies.js
+++ routes/movies.js
@@ -44,7 +44,7 @@ router.put("/:id", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   const genre = await Genre.findById(req.body.genreId);
-  console.log(genre.name);
+
   if(!genre) return res.status(404).send('Invalid genre');
 
   const movie = await Movie.findByIdAndUpdate(req.params.id, {
